62e45e669de65ac3632ae3db3aa098e4716a72d3
Criscardozo94 <158121760+Criscardozo94@users.noreply.github.com>
2024-02-18 20:07:16 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const libros = [
+    {id: '1', titulo: 'Libro 1', autor: 'Autor A'},
+    {id: '2', titulo: 'Libro 2', autor: 'Autor B'},
+    {id: '3', titulo: 'Libro 3', autor: 'Autor C'}
+];
+
+module.exports = libros;
